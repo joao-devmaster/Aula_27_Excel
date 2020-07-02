f03a02949bd16a90369112d0aa2efb56cc7692a1
joao-devmaster <joaovitorsantossouza7@gmail.com>
2020-07-02 00:11:57 -0300

    atividade 29 finalizada

--- Produto.cs
+++ Produto.cs
@@ -1,121 +1,148 @@
-
-using System;
 using System.Collections.Generic;
 using System.IO;
+using System;
 
-namespace Aula27_dados_excel
+namespace Aula27_28_29_30
 {
     public class Produto
     {
-           public int Codigo {get;set;}
+        
+        public int Codigo {get;set;}
         public string Nome {get;set;}
         public float Preco {get;set;}
 
         private const string PATH = "Database/Produto.csv";
 
-        //criando pasta Database manualmente
         public Produto()
         {
 
-            if(!File.Exists(PATH))
+            //DESAFIO
+            string pasta = PATH.Split('/')[0]; //Criando array com o arquivo Database
+
+            if(!Directory.Exists(pasta))//Se o diretório não existe, crie o diretório
+            {
+                Directory.CreateDirectory(pasta);
+            }
+
+
+            if(!File.Exists(PATH))//criando pasta Database manualmente
             {
                 File.Create(PATH).Close();
             }
         
         }
 
+        /// <summary>
+        /// Adicionando novos produtos
+        /// </summary>
+        /// <param name="p"></param>
         public void Inserir(Produto p){
             var linha = new string [] {p.PrepararLinhaCSV(p)};
             File.AppendAllLines(PATH, linha);
         }
 
+        /// <summary>
+        /// Organizar os dados para ficarem em linhas (codigo=x; nome=a; preco=0)
+        /// </summary>
+        /// <param name="produto"></param>
+        /// <returns></returns>
         private string PrepararLinhaCSV(Produto produto){
 
             return $"Cod= {produto.Codigo}; Nome = {produto.Nome}; Preco = {produto.Preco}";
         }
 
+        //-------------------------------------------------------
+        //Aula28 - Ler Dados Excel
 
+        public List<Produto> Ler(){
 
-
-       
-       // 2 aula  
-        private string Separar(string _coluna)
-        {
-            // 0         1
-            //nome   =  gibson
-            return _coluna.Split("=") [1];
-        }
-
-        // 1; celular;600
-        private string PrepararLinha(Produto p)
-        {
-            return $"codigo={p.Codigo};nome={p.Nome};={p.Preco}";
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-        public List<Produto> Ler()
-        {
             //Criamos uma lista de produtos
-        List<Produto> produtos = new List<Produto>();
+            List<Produto> produto1 = new List<Produto>();
 
-        // Transformamos as linhas encontradas em um array de strings
-        string[] linhas = File.ReadAllLines(PATH);
+            //Transformamos as linhas encontradas em uma array de strings
+            string [] lista = File.ReadAllLines(PATH);
 
-        // Varremos este array de strings
-        foreach (var linha in linhas)
-        {
-             // quebramos cada linha em partes, pegando
-             string[] dados = linha.Split(";");
+            //Varremos este array de strings
+            foreach(var linha in lista){
 
-             // Tratamos os dados e adicionamos em um novo produto
-             Produto prod = new Produto();
-             prod.Codigo = Int32.Parse( Separar(dados[0]) );
-             prod.Nome = Separar(dados[1]);
-             prod.Preco = float.Parse( Separar(dados[2]) );
-             
+                //Quebramos cada linha da lista em partes
+                string[] dados = linha.Split(';'); //dividindo a string em cada ;
 
-             // agora adicionamos o produto tratado na lista de produtos a ntes de retorná-la
-             produtos.Add(prod);
+                //Tratamento dos dados e adicionar um novo produto
+                Produto prod = new Produto();
+                prod.Codigo = Int32.Parse (SepararDados(dados[0]));
+                prod.Nome   = SepararDados(dados[1]);
+                prod.Preco  = float.Parse (SepararDados(dados[2]));
 
-        } 
-        return produtos;
+                //Adicionar o produto tratado na lista                
+                produto1.Add(prod);
+
+            }
+
+            return produto1;
 
         }
 
-       
- 
-        
+        /// <summary>
+        /// Remove uma ou mais linhas que contenham o termo
+        /// </summary>
+        /// <param name="_termo">termo para ser buscado</param>
+        public void Remover(string _termo){
+
+            // Criamos uma lista que servirá como uma espécie de backup para as linhas do csv
+            List<string> linhas = new List<string>();
+
+            // Utilizamos a bliblioteca StreamReader para ler nosso .csv
+            using(StreamReader arquivo = new StreamReader(PATH))
+            {
+                string linha;
+                while((linha = arquivo.ReadLine()) != null)
+                {
+                    linhas.Add(linha);
+                }
+            }
+
+            // Removemos as linhas que tiverem o termo passado como argumento
+            // Cod= 1; Nome = AudioBook Revolta de Atlas; Preco = 21,98
+            // AudioBook Revolta de Atlas 
+            linhas.RemoveAll(l => l.Contains(_termo));
+
+            // Reescrevemos nosso csv do zero
+            using(StreamWriter output = new StreamWriter(PATH))
+            {
+                foreach(string ln in linhas)
+                {
+                    output.Write(ln + "\n");
+                }
+            }
+        }
 
 
 
-
-        
-
-        
-
-         
-
-
-
-       
-        
-   
+        /// <summary>
+        /// Busca de produto
+        /// </summary>
+        /// <param name="_nome"></param>
+        /// <returns></returns>
+        public List<Produto> Filtrar(string _nome){
+            return Ler().FindAll( a => a.Nome == _nome);
+        }
 
 
+        /// <summary>
+        /// Tratamento da array lista para obter somente nome, preço e código
+        /// </summary>
+        /// <param name="_coluna"></param>
+        /// <returns></returns>
+        private string SepararDados(string _coluna){
+            
+            //cada informação esta assim
+            //0       1
+            //Preco = The Last Of Us II
+            return _coluna.Split("=")[1];
 
+        }
 
-
-
-
+    
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,31 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
-using System;
-
-namespace Aula27_dados_excel
+namespace Aula27_28_29_30
 {
     class Program
     {
         static void Main(string[] args)
         {
-             Produto p = new Produto();
-            p.Codigo = 1;
-            p.Nome = "AudioBook Revolta de Atlas";
-            p.Preco = 21.98f;
+            //instanciando um novo produto que será armazenado na database/produto.csv
+            Produto p = new Produto();
+            p.Codigo = 2;
+            p.Nome = "11 Cidades - Axel Torres";
+            p.Preco = 49.98f;
 
+            //Inserindo produto 
             p.Inserir(p);
 
-       List<Produto> lista1 = new List<Produto>();
-        lista1 = p.Ler();
+            //Remover todos os produtos com esse nome
+            p.Remover("11 Cidades - Axel Torres");
 
-        foreach (Produto item in lista1)
-        {
-            System.Console.WriteLine($"R$ {item.Preco} - {item.Nome} ");
+            //Instanciando a lista de produto
+            List<Produto> lista1 = new List<Produto>();
+            lista1 = p.Ler();
 
+            foreach(Produto item in lista1){
+                Console.WriteLine($"Produto: {item.Nome} custa R$ {item.Preco}");
+            }
+
+            
         }
-
-         
-        }
-
+    
+    
     }
 }
